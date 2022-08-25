4391d8270790bc814f353c8695b5c9bc5c50f449
Margarita <margarita27011991@gmail.com>
2022-08-25 12:26:54 +0300

    сколько чисел больше 0 ввёл пользователь

--- task41/Program.cs
+++ task41/Program.cs
@@ -1,3 +1,26 @@
 ﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3    */
+Console.Clear();
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int [] arr = new int [size];
+    for(int i = 0; i <arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange +1);
+    }
+    return arr;
+}
+
+int[] array = FillArray (8,-100, 100);
+Console.WriteLine('['+ string.Join(", ", array) + ']');
+int count = 0;
+for(int i = 0; i <array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
