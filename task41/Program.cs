509390d9efff709fd9d478b53ac0428b52b914f9
Margarita <margarita27011991@gmail.com>
2022-08-26 20:00:58 +0300

    сколько чисел больше 0 ввел пользователь

--- task41/Program.cs
+++ task41/Program.cs
@@ -2,17 +2,26 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3    */ 
 Console.Clear();
-int[] FillArray(int size, int leftRange, int rightRange)
-{
-    Random rand = new Random();
-    int [] arr = new int [size];
-    for(int i = 0; i <arr.Length; i++)
-    {
-        arr[i] = rand.Next(leftRange, rightRange +1);
-    }
-    return arr;
+// int[] FillArray(int size, int leftRange, int rightRange)
+// {
+//     Random rand = new Random();
+//     int [] arr = new int [size];
+//     for(int i = 0; i <arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(leftRange, rightRange +1);
+//     }
+//     return arr;
+// }
+// int[] array = FillArray (11,-100, 100);
+Console.WriteLine("Введите длину массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+{   
+    Console.WriteLine("Введите число");
+    int a = Convert.ToInt32(Console.ReadLine());
+    array[0 + i] = a;
 }
-int[] array = FillArray (8,-100, 100);
 Console.WriteLine('['+ string.Join(", ", array) + ']');
 int count = 0;
 for(int i = 0; i <array.Length; i++)
