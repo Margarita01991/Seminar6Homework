843c6a2cb64c886f75968b2f844fc51e2c85cac9
Margarita <margarita27011991@gmail.com>
2022-08-26 20:29:29 +0300

    создать копию заданного массива с помощью поэлементного копирования

--- task45/Program.cs
+++ task45/Program.cs
@@ -1,13 +1,12 @@
 ﻿//Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
 int[] array = {3, 5, -2, 7, 83};
 int N = array.Length;
+Console.Write ("Заданный массив: ");
 Console.WriteLine('['+ string.Join(", ", array) + ']');
 int[] array2 = new int[N];
-Console.Write("[");
 for (int i = 0; i < array.Length; i++)
 {
     array2[i] = array[i];
-    Console.Write(array2[i]);
-    Console.Write(",");
 }
-Console.Write("]");
+Console.Write ("Копия массива:   ");
+Console.WriteLine('['+ string.Join(", ", array2) + ']');
